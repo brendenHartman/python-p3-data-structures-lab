85bd74ce777e72c9d34a0b070f8f4ebd9960a042
Brenden Hartman <brendenhart22@gmail.com>
2024-04-25 15:22:28 -0500

    practiced working with pyuthon data structures using different iterations and comprehension now includding dictionaries

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,37 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [name["name"] for name in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for item in spicy_foods:
+        name = item["name"]
+        cuisine = item["cuisine"]
+        heat = item["heat_level"]
+        peps = "🌶" * heat
+        print(f"{name} ({cuisine}) | Heat Level: {peps}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    found = [item for item in spicy_foods if cuisine == item["cuisine"]]
+    return found[0]
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    food_printed = [food for food in spicy_foods if food["heat_level"] > 5]
+    print_spicy_foods(food_printed)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    avg = 0
+    count = 0
+    for food in spicy_foods:
+        avg += food["heat_level"]
+        count += 1
+    avg = avg / count
+    return avg
+            
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
